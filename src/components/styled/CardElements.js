c06523f6e7b1fd3349559dba03d04d7abaf98d0f
8bitago <eightbitago@gmail.com>
2020-11-11 12:23:48 +0300

    Added padding-bottom

--- src/components/styled/CardElements.js
+++ src/components/styled/CardElements.js
@@ -4,6 +4,7 @@ export const CardWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  padding-bottom: 3rem;
 `;
 
 export const CardBottomWrapper = styled.div`
