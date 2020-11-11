97610b9a3e44e39be33e8921dfe1c48bf6ce612d
8bitago <eightbitago@gmail.com>
2020-11-11 12:32:05 +0300

    Displayed post from mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,37 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import {
-  Container, Content, Header, Navbar, Sidebar, Social,
+  Container, Content, Header, Navbar, Post, Sidebar, Social,
 } from '../components';
 
-const IndexPage = () => (
-  <Container>
-    <Header />
-    <Sidebar>
-      <Navbar />
-      <Social />
-    </Sidebar>
-    <Content>
-      <h1>Hello, World</h1>
-    </Content>
-  </Container>
-);
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      mdx(frontmatter: {slug: {eq: "page-about"}}) {
+        body
+        frontmatter {
+          slug
+          title
+          excerpt
+        }
+      }
+    }
+  `);
+
+  return ((
+    <Container>
+      <Header />
+      <Sidebar>
+        <Navbar />
+        <Social />
+      </Sidebar>
+      <Content>
+        <Post title={data.mdx.frontmatter.title}>
+          { data.mdx.body }
+        </Post>
+      </Content>
+    </Container>
+  ));
+};
 
 export default IndexPage;
